0c32d68fabc7f64f90625d24e2e44d93a38df3d9
manolo <manolo@idealis.es>
2018-10-03 19:36:27 +0200

    welcome

--- welcome.php
+++ welcome.php
@@ -1,75 +1,47 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>CreativeChain</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Site Description Here">
-        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/stack-interface.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/socicon.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/lightbox.min.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/flickity.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/iconsmind.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/jquery.steps.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/theme.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/custom/creativechain.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="css/font-roboto.css" rel="stylesheet" type="text/css" media="all" />
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700%7CMerriweather:300,300i" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,700" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    </head>
-    <body class=" ">
-        <a id="start"></a>
-
-        <div class="main-container">
-            <section class="cover height-100 imagebg switchable siwtchable--switch" data-overlay="8">
-                <div class="container pos-vertical-center">
-                    <div class="row justify-content-around" style="height: 100vh;">
-                        <div class="col-lg-5 col-md-7">
-                            <div class="switchable__text">
-                                <h2>Descubre la comunidad crea!</h2>
-                                <p class="">
-                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
-                                </p>
-                                <div class="row justify-content-md-center">
-                                    <div class="col-md-4">
-                                        <a class="btn btn--primary" href="#">
-                                            <span class="btn__text">Inscríbete</span>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-4 col-12">
-                            <div class="slide-img-1 position-img-slide">
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+<?php include ('element/navbar.php'); ?>
+<div class="main-container view-welcome">
+    <section class="imageblock switchable height-100">
+        <div class="imageblock__content col-lg-6 col-md-4 pos-right">
+            <div class="background-image-holder">
+                <img alt="image" src="img/welcome/slide-1@2x.png" class="logo-welcome" />
+            </div>
         </div>
-        <!--<div class="loader"></div>-->
-        <a class="back-to-top inner-link" href="#start" data-scroll-class="100vh:active">
-            <i class="stack-interface stack-up-open-big"></i>
-        </a>
-        <script src="js/jquery-3.1.1.min.js"></script>
-        <script src="js/flickity.min.js"></script>
-        <script src="js/easypiechart.min.js"></script>
-        <script src="js/parallax.js"></script>
-        <script src="js/typed.min.js"></script>
-        <script src="js/datepicker.js"></script>
-        <script src="js/isotope.min.js"></script>
-        <script src="js/ytplayer.min.js"></script>
-        <script src="js/lightbox.min.js"></script>
-        <script src="js/granim.min.js"></script>
-        <script src="js/jquery.steps.min.js"></script>
-        <script src="js/countdown.min.js"></script>
-        <script src="js/twitterfetcher.min.js"></script>
-        <script src="js/spectragram.min.js"></script>
-        <script src="js/smooth-scroll.min.js"></script>
-        <script src="js/scripts.js"></script>
-    </body>
-</html>
+        <div class="col-lg-6 col-md-6 offset-lg-6 offset-md-6 no-padding">
+            <img src="img/welcome/logo-welcome.png" alt="">
+        </div>
+        <div class="container pos-vertical-center content-slide-welcome no-padding">
+
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <h1>Descubre la comunidad Crea!</h1>
+                    <p class="lead">Creary es la comunidad creativa descentralizada conectada a la blockchain de Crea.  No somos una red social convencional controlada por una entidad centralizada.</p>
+                    <p class="lead">La blockchain de Crea se sostienen de manera transversal por los miembros de la comunidad de todo el mundo.</p>
+                    <p class="lead">Para poder crear una cuenta en Creary se cobra una micro tarifa para evitar el registro masivo de bots o cuentas malintencionadas. Con mucho gusto Creary cubrirá esta tarifa, solo necesitaremos su correo electrónico con el objetivo de poder verificar que usted es una persona real.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 text-center">
+                    <a class="btn btn--primary type--uppercase" href="#">
+                        <span class="btn__text">
+                            Inscribirse
+                        </span>
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 text-center">
+                    <ul class="list-inline list-unstyled">
+                        <li><p class="circle-welcome active"></p></li>
+                        <li><p class="circle-welcome"></p></li>
+                        <li><p class="circle-welcome"></p></li>
+                        <li><p class="circle-welcome"></p></li>
+                        <li><p class="circle-welcome"></p></li>
+                    </ul>
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </section>
+</div>
+<?php include ('element/footer.php'); ?>
