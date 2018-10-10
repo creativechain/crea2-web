a5f11b16a9312a625fd8005e7f9566b1942dcd99
manolo <manolo@idealis.es>
2018-10-10 10:23:47 +0200

    create scss publish

--- publish.php
+++ publish.php
@@ -1,37 +1,70 @@
 <?php include ('element/navbar.php'); ?>
-    <div class="main-container">
-    <section class="imagebg image--light cover cover-blocks bg--secondary" id="slide-home">
-        <div class="background-image-holder hidden-xs">
-            <img alt="background" src="img/crea-web/welcome/banner-home.jpg"/>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-lg-5 ">
-                    <div>
+    <div class="main-container view-publish">
+        <section class="imagebg image--light cover cover-blocks bg--secondary" id="slide-home">
+            <div class="background-image-holder hidden-xs">
+                <img alt="background" src="img/crea-web/welcome/banner-home.jpg"/>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-lg-5 ">
+                        <div>
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <section class="cta cta-4 space--xxs border--bottom ">
-        <div class="container">
-            <div class="row">
-                <?php include ('modules/navbar-filter-home.php') ?>
-            </div>
-        </div>
-    </section>
-
-
-    <section class="space--sm">
-        <div class="container">
-            <div class="masonry">
-                <div class="masonry__container row">
-                    <?php include ('modules/post-view-home.php') ?>
+        </section>
+        <section class="space--sm">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-12 col-lg-8">
+                        <div class="wizard">
+                            <h5>Contenido</h5>
+                            <section>
+                                <h4 class="title-steps">Contenido</h4>
+                                <span class="description-step-title">Empieza a construir tu proyecto</span>
+                                <div class="pos-vertical-center text-center">
+                                    <div class="row">
+                                        <div class="col-md-4 offset-4">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <img src="img/crea-web/publish/b-img.png" alt="">
+                                                    <p>Archivo</p>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <img src="img/crea-web/publish/b-img.png" alt="">
+                                                    <p>Texto</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                            <h5>Información</h5>
+                            <section class="text-center">
+                                <div class="pos-vertical-center">
+                                    <h4>Navigate the steps using your keyboard arrows</h4>
+                                </div>
+                            </section>
+                            <h5>Descarga</h5>
+                            <section class="text-center">
+                                <div class="pos-vertical-center">
+                                    <h4>Use Wizards for onboarding, form data collection and more</h4>
+                                </div>
+                            </section>
+                            <h5>Licencia</h5>
+                            <section class="text-center">
+                                <div class="pos-vertical-center">
+                                    <h4>Use Wizards for onboarding, form data collection and more</h4>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
                 </div>
+                <!--end of row-->
             </div>
-        </div>
-    </section>
+            <!--end of container-->
+        </section>
 
 
 <?php include ('element/footer.php'); ?>
