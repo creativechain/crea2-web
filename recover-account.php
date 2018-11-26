fa59dc6a6941dbb3d832b633da3682fee790fda7
manolo <manolo@idealis.es>
2018-11-26 20:11:27 +0100

    nueva vista recover account

--- recover-account.php
+++ recover-account.php
@@ -10,14 +10,17 @@
                 </div>
                 <form class="row">
                     <div class="col-md-12">
-                        <p>Muestra tus proyectos artísticos a nuestra comunidad creativa descentralizada</p>
+                        <p class="lead">If someone manages to hack your account by accessing the place where you saved your password, he could change it and prevent access to the original owner of the account.</p>
+                        <p class="lead">The blockchain of Crea incorporates a system for recovering stolen accounts. If your password has been changed without your consent, then the account designated as your recovery account can generate a new owner key for the account.</p>
+                        <p class="lead">This recovery process must be completed within 30 days after the password is modified, and the user must provide a recent owner key that has been valid in the last 30 days.</p>
+                        <p class="lead">This functionality can only be used in creary.net as long as the owner of the account has included Creary as his trusted account and has also accepted the terms and conditions of service of Creary.net.</p>
                     </div>
-                    <div class="col-md-12">
-                        <label>Your Name:</label>
+                    <div class="col-md-12 mt-4">
+                        <label class="lead mb-0">Your Name:</label>
                         <input type="text" name="name" placeholder="First / Last Name" class="validate-required">
                     </div>
                     <div class="col-md-12">
-                        <label>Email Address:</label>
+                        <label class="lead mb-0">Email Address:</label>
                         <input type="email" name="email" placeholder="you@something.com" class="validate-required">
                     </div>
                     <div class="col-md-12 mt-3">
