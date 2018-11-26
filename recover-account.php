0d86153c625aa102faeef01058314cdd44849b7b
manolo <manolo@idealis.es>
2018-11-26 12:54:37 +0100

    new view recover account

---
+++ recover-account.php
@@ -0,0 +1,35 @@
+<?php include ('element/navbar.php'); ?>
+<div class="main-container view-recorver-account">
+    <section class="space--sm">
+        <div class="container post-container-home">
+            <div class="col-md-8 offset-2">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="title-explorer">Recuperar contraseña </h3>
+                    </div>
+                </div>
+                <form class="row">
+                    <div class="col-md-12">
+                        <p>Muestra tus proyectos artísticos a nuestra comunidad creativa descentralizada</p>
+                    </div>
+                    <div class="col-md-12">
+                        <label>Your Name:</label>
+                        <input type="text" name="name" placeholder="First / Last Name" class="validate-required">
+                    </div>
+                    <div class="col-md-12">
+                        <label>Email Address:</label>
+                        <input type="email" name="email" placeholder="you@something.com" class="validate-required">
+                    </div>
+                    <div class="col-md-12 mt-3">
+                        <a id="welcome-slide1-button-signup" href="#" class="btn btn--primary">
+                            <span class="btn__text">
+                                Sign up
+                            </span>
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+
+<?php include ('element/footer.php'); ?>
