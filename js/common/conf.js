860b58bd2547dcf29c73f4dfce3308c2de537d84
ander7agar <ander7agar@gmail.com>
2018-12-07 21:32:59 +0100

    fixing crea node

--- js/common/conf.js
+++ js/common/conf.js
@@ -5,7 +5,7 @@
 
 let localNode = 'http://cn.localhost';
 let apiOptions = {
-    url: true ? localNode : 'https://crea.owldevelopers.site',
+    url: false ? localNode : 'https://crea.owldevelopers.site',
     ipfs: 'https://ipfs.creary.net',
     addressPrefix: 'CREA',
     symbol: {
