39a736916d0dc15913fa3ffcb167e4d7f7a5e08d
manolo <manolo@idealis.es>
2018-12-21 10:56:03 +0100

    faq

---
+++ faq.php
@@ -0,0 +1,205 @@
+<?php include ('element/navbar.php'); ?>
+
+
+<div class="main-container">
+    <section class="space--sm">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 hidden-sm">
+                    <div class="sidebar boxed boxed--border boxed--lg bg--secondary">
+                        <div class="sidebar__widget">
+                            <h5>Search site</h5>
+                            <form>
+                                <input type="text" name="query" placeholder="Type search here">
+                            </form>
+                        </div>
+                        <!--end widget-->
+                        <div class="sidebar__widget">
+                            <h5>Text Widget</h5>
+                            <p>
+                                Our new digital products will take your workflow to all-new levels of high productivity. We know you'll find everything you need - and more! Start building with Stack.
+                            </p>
+                        </div>
+                        <!--end widget-->
+                        <div class="sidebar__widget">
+                            <h5>Tags</h5>
+                            <ul class="tag-cloud">
+                                <li>
+                                    <a href="#">Freelancing</a>
+                                </li>
+                                <li>
+                                    <a href="#">Design</a>
+                                </li>
+                                <li>
+                                    <a href="#">Small Business</a>
+                                </li>
+                                <li>
+                                    <a href="#">Entrepreneurship</a>
+                                </li>
+                                <li>
+                                    <a href="#">Marketing</a>
+                                </li>
+                                <li>
+                                    <a href="#">Inspiration</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!--end widget-->
+                        <div class="sidebar__widget">
+                            <h5>Categories</h5>
+                            <ul>
+                                <li>
+                                    <a href="#">Freelancing</a>
+                                </li>
+                                <li>
+                                    <a href="#">Design</a>
+                                </li>
+                                <li>
+                                    <a href="#">Small Business</a>
+                                </li>
+                                <li>
+                                    <a href="#">Entrepreneurship</a>
+                                </li>
+                                <li>
+                                    <a href="#">Marketing</a>
+                                </li>
+                                <li>
+                                    <a href="#">Inspiration</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!--end widget-->
+                        <div class="sidebar__widget">
+                            <h5>Archives</h5>
+                            <ul>
+                                <li>
+                                    <a href="#">August 2016</a>
+                                </li>
+                                <li>
+                                    <a href="#">July 2016</a>
+                                </li>
+                                <li>
+                                    <a href="#">June 2016</a>
+                                </li>
+                                <li>
+                                    <a href="#">May 2016</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!--end widget-->
+                    </div>
+                </div>
+                <div class="col-md-12 col-lg-8">
+                    <ul class="accordion accordion-1">
+                        <li class="active">
+                            <div class="accordion__title">
+                                <span class="h5">Panel One</span>
+                            </div>
+                            <div class="accordion__content">
+                                Content
+                            </div>
+                        </li>
+                        <li>
+                            <div class="accordion__title">
+                                <span class="h5">Panel Two</span>
+                            </div>
+                            <div class="accordion__content">
+                                Content
+                            </div>
+                        </li>
+                    </ul>
+                    <!--end masonry-->
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </section>
+    <footer class="footer-3 text-center-xs space--xs ">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img alt="Image" class="logo" src="img/logo-dark.png">
+                    <ul class="list-inline list--hover">
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <span class="type--fine-print">Get Started</span>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <span class="type--fine-print">help@stack.io</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-6 text-right text-center-xs">
+                    <ul class="social-list list-inline list--hover">
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <i class="socicon socicon-google icon icon--xs"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <i class="socicon socicon-twitter icon icon--xs"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <i class="socicon socicon-facebook icon icon--xs"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#">
+                                <i class="socicon socicon-instagram icon icon--xs"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!--end of row-->
+            <div class="row">
+                <div class="col-md-6">
+                    <p class="type--fine-print">
+                        Supercharge your web workflow
+                    </p>
+                </div>
+                <div class="col-md-6 text-right text-center-xs">
+                            <span class="type--fine-print">©
+                                <span class="update-year">2018</span> Stack Inc.</span>
+                    <a class="type--fine-print" href="#">Privacy Policy</a>
+                    <a class="type--fine-print" href="#">Legal</a>
+                </div>
+            </div>
+            <!--end of row-->
+        </div>
+        <!--end of container-->
+    </footer>
+</div>
+
+
+
+
+<div class="main-container">
+    <ul class="accordion accordion-1">
+        <li class="active">
+            <div class="accordion__title">
+                <span class="h5">Panel One</span>
+            </div>
+            <div class="accordion__content">
+                Content
+            </div>
+        </li>
+        <li>
+            <div class="accordion__title">
+                <span class="h5">Panel Two</span>
+            </div>
+            <div class="accordion__content">
+                Content
+            </div>
+        </li>
+    </ul>
+
+
+    <?php include ('element/footer.php'); ?>
--- index2.php
+++ index2.php
@@ -10,6 +10,7 @@ $router->addRoute('/welcome', 'welcome.php');
 $router->addRoute('/validate', 'welcome.php');
 $router->addRoute('/publish', 'publish.php');
 $router->addRoute('/explore', 'explorer.php');
+$router->addRoute('/faq', 'faq.php');
 $router->addRoute('/recover-account', 'recover-account.php');
 $router->addRoute('/404', '404.php');
 $router->addRoute('^\/([\w\d\-\/]+)\/?$', 'home.php'); //
