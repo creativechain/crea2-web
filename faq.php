5e84544e609b8702ae2c6fa2cba1ec97e207e140
ander7agar <ander7agar@gmail.com>
2018-12-28 19:16:47 +0100

    added faq

--- faq.php
+++ faq.php
@@ -1,17 +1,30 @@
 <?php include ('element/navbar.php'); ?>
 
 
+<script src="/language/faq-es.js"></script>
 <div class="main-container view-faq">
-    <section>
+    <section v-cloak id="faq-container">
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-lg-7">
                     <ul class="results-list">
-                        <li>
-                            <h4>¿Qué es creary.net?</h4>
-                            <p>Creary es una red social de portfolios multimedia basada en la blockchain que recompensa a creativos y curadores por compartir sus creaciones digitales con la comunidad.</p>
-                        </li>
-                        <li>
+                        <template v-for="cat in faq.CATEGORIES">
+
+                            <li v-for="q in faq.QUESTIONS[cat]">
+                                <h4 v-bind:id="toPermalink(q)">{{ q }}</h4>
+
+                                <template v-for="r in faq.RESPONSES[cat][q]">
+                                    <p v-if="linkfy(r)" v-html="linkfy(r)"></p>
+                                    <p v-else>
+                                        {{ r }}
+                                    </p>
+                                </template>
+
+                            </li>
+
+                        </template>
+
+<!--                        <li>
                             <h4>¿Cómo funciona?</h4>
                             <p>Creary es la primera aplicación que funciona sobre la blockchain de Crea. En Creary cualquiera puede publicar contenidos digitales y ser recompensado por su trabajo creativo según los votos recibidos por parte de la comunidad.</p>
                             <p>Los usuarios son recompensados con criptomonedas llamadas CREA.<p>
@@ -24,25 +37,25 @@
                             <h4>¿ Por qué Creary es distinto a otras redes sociales convencionales ?</h4>
                             <p>En internet, la mayoría de las red sociales convencionales son centralizadas, esto quiere decir que unos pocos participan del control de la red social, de los contenidos y de la economía. En muchos casos el negocio consiste en vender los datos de los usuarios a terceros que ofrecen publicidad. Sólo unos pocos son accionistas.</p>
                             <p>En Creary creemos que el poder y los beneficios se deberían de diluir entre todos los usuarios que aportan valor a la red. El 100% de los tokens emitidos por la red se distribuyen entre los usuarios de la misma. El sistema de distribución de recompensas está gobernado por la propia comunidad y cualquier usuario puede votar libremente y a tiempo real sobre cómo quiere que evolucione la red de Crea.</p>
-                        </li>
+                        </li>-->
                     </ul>
                 </div>
                 <div class="col-md-pull-8 col-md-4">
                     <div class="boxed boxed--border boxed--lg bg--secondary">
                         <div class="sidebar__widget">
-                            <h5>La base</h5>
-                            <ul>
-                                <li><a href="#">¿Qué es creary.net?</a></li>
-                                <li><a href="#">¿Cómo funciona?</a></li>
-                                <li><a href="#">¿ Por qué Creary es distinto a otras redes sociales convencionales?</a></li>
-                                <li><a href="">¿Cómo debo proteger mi cuenta?</a></li>
-                                <li><a href="">¿Cuesta algo publicar, comentar o votar?</a></li>
-                                <li><a href="">¿Cómo ganar criptomonedas usando Creary?</a></li>
-                                <li><a href="">¿Cómo se generan los nuevos tokens?</a></li>
-                                <li><a href="">¿De dónde viene el valor de los tokens en la red de Crea?</a></li>
+                            <template v-for="cat in faq.CATEGORIES">
+                                <h5>{{ cat }}</h5>
 
-                            </ul>
-                            <h5>Cuentas</h5>
+                                <ul>
+                                    <li v-for="q in faq.QUESTIONS[cat]">
+                                        <a v-bind:href="'#' + toPermalink(q)">{{ q }}</a>
+                                    </li>
+                                </ul>
+
+                            </template>
+
+
+<!--                            <h5>Cuentas</h5>
                             <ul>
                                 <li><a href="#">¿Cómo creo una cuenta?</a></li>
                                 <li><a href="#">¿Por qué tengo que proporcionar mi correo electrónico?</a></li>
@@ -50,116 +63,8 @@
                                 <li><a href="">¿Qué sucede si mi correo electrónico cambia?</a></li>
                                 <li><a href="">¿Puedo cambiar mi nombre de usuario?</a></li>
                                 <li><a href="">¿Puedo eliminar o desactivar mi cuenta?</a></li>
-                            </ul>
-                            <h5>Navegación</h5>
-                            <ul>
-                                <li><a href="#">¿Cómo voto una publicación o comentario en Creary?</a></li>
-                                <li><a href="">¿Qué muestran los enlaces Following, Popular, Skyrockets, Now, Promoted?</a></li>
-                                <li><a href="">¿Cómo veo mis recompensas recientes?</a></li>
-                                <li><a href="">¿Qué información se muestra en mi monedero?</a></li>
-                                <li><a href="">¿Cómo transfiero mi CREA o CGY a mis ahorros?</a></li>
-                                <li><a href="">¿Cómo envío dinero a otro usuario?</a></li>
-                                <li><a href="">¿Recibiré notificaciones cuando haya actividad con mi cuenta?</a></li>
-                            </ul>
-                            <h5>Portfolio</h5>
-                            <ul>
-                                <li><a href="">¿Qué se puede publicar en Creary?</a></li>
-                                <li><a href="">¿Cómo publicar un trabajo?</a></li>
-                                <li><a href="">¿Qué son las etiquetas?</a></li>
-                                <li><a href="">¿Por qué el botón "continuar" en el proceso de publicación aparece en gris?</a></li>
-                                <li><a href="">¿Con qué frecuencia puedo publicar?</a></li>
-                                <li><a href="">¿Puedo borrar algo que publiqué?</a></li>
-                                <li><a href="">¿Qué licencia debo escoger al publicar mi contenido?</a></li>
-                                <li><a href="">¿Dónde se almacenan los textos y archivos que subo desde Creary.net ?</a></li>
-                                <li><a href="">¿Qué hace "Promover" una publicación?</a></li>
-                            </ul>
-                            <h5>Economía</h5>
-                            <ul>
-                                <li><a href="">¿De dónde vienen los nuevos tokens CREA?</a></li>
-                                <li><a href="">¿Cuántos nuevos tokens genera la cadena de bloques?</a></li>
-                                <li><a href="">¿Cómo se distribuyen los nuevos tokens que se generan en cada bloque?</a></li>
-                                <li><a href="">¿En qué intercambios puedes encontrar CREA?</a></li>
-                                <li><a href="">¿Cómo se divide el grupo de recompensas entre autores y curadores?</a></li>
-                                <li><a href="">¿El grupo de recompensas pagará más o menos dependiendo de quién vote?</a></li>
-                                <li><a href="">¿Cuándo puedo reclamar mis recompensas?</a></li>
-                                <li><a href="">¿Cuál es la diferencia entre CREA, CREA Energy y CREA Dollars?</a></li>
-                                <li><a href="">¿Qué es el CREA Energy delegado?</a></li>
-                                <li><a href="">¿Qué determina el precio de CREA?</a></li>
-                                <li><a href="">¿Cómo obtengo más CREA Energy?</a></li>
-                                <li><a href="">¿Qué es Energize y De-Energize ?</a></li>
-                                <li><a href="">¿Cuánto tiempo lleva la transferencia de tokens entre usuarios dentro de la red de Crea?</a></li>
-                                <li><a href="">¿Qué representan los importes en dólares para pagos pendientes?</a></li>
-                                <li><a href="">¿1 CREA dollar (CBD) siempre valdrá $ 1.00 USD?</a></li>
-                                <li><a href="">¿Qué puedo hacer con mis tokens de CREA?</a></li>
-                                <li><a href="">¿Qué puedo hacer con mis tokens CBD?</a></li>
-                                <li><a href="">¿Cuánto cuestan las transacciones de CREA a otros usuarios?</a></li>
-                            </ul>
-                            <h5>Votación y Curating</h5>
-                            <ul>
-                                <li><a href="">¿Cuál es mi poder de voto?</a></li>
-                                <li><a href="">¿Cuántas veces puedo votar sin agotar mi poder de voto?</a></li>
-                                <li><a href="">¿Puedo votar con menos del 100% de mi poder de voto?</a></li>
-                                <li><a href="">¿Dónde puedo verificar mi poder de voto?</a></li>
-                                <li><a href="">¿Qué determina qué parte de la recompensa de curación va para el autor frente a los curadores?</a></li>
-                                <li><a href="">¿Recibiré recompensas de curaduría por denunciar, publicaciones o comentarios?</a></li>
-                                <li><a href="">¿Por qué mis votaciones no tienen efecto en las recompensas de una publicación?</a></li>
-                                <li><a href="">¿Hay alguna manera de conseguir que mis votos tengan más influencia? </a></li>
-                                <li><a href="">¿Cuáles son los motivos válidos para realizar un voto negativo al reportar un contenido?</a></li>
-                            </ul>
-                            <h5>Plagio, spam y abuso</h5>
-                            <ul>
-                                <li><a href="">¿Cuales son las políticas sobre el plagio o la violación de los derechos de autor?</a></li>
-                                <li><a href="">¿Dónde puedo informar sobre autores, publicaciones o comentarios que contienen plagio, spam o abuso de algún tipo?</a></li>
-                            </ul>
-                            <h5>Reputación</h5>
-                            <ul>
-                                <li><a href="">¿Qué es el Buzz en Creary?</a></li>
-                                <li><a href="">¿Qué causa que mi puntaje de Buzz baje?</a></li>
-                                <li><a href="">¿Por qué mi nivel de Buzz es importante?</a></li>
-                            </ul>
-                            <h5>Seguidores, Feeds y Recreary</h5>
-                            <ul>
-                                <li><a href="">¿Qué es Recreary?</a></li>
-                            </ul>
-                            <h5>Tecnología detrás de Creary</h5>
-                            <ul>
-                                <li><a href="">¿Qué es una blockchain?</a></li>
-                                <li><a href="">¿Qué es la blockchain de Crea?</a></li>
-                                <li><a href="">¿Con qué frecuencia la blockchain de Crea produce un nuevo bloque?</a></li>
-                                <li><a href="">¿Hay alguna manera de ver los datos sin procesar que se almacenan en la cadena de bloques?</a></li>
-                                <li><a href="">¿Dónde puedo encontrar  más información sobre el funcionamiento de la blockchain de Crea ?</a></li>
-                            </ul>
-                            <h5>Creativechain Foundation</h5>
-                            <ul>
-                                <li><a href="">¿Qué es Creativechain Foundation?</a></li>
-                                <li><a href="">¿Puedo invertir en Creativechain Foundation?</a></li>
-                                <li><a href="">¿Puedo usar el logotipo de Creary?</a></li>
-                            </ul>
-                            <h5>Seguridad</h5>
-                            <ul>
-                                <li><a href="">¿Cómo puedo mantener segura mi cuenta de Crea?</a></li>
-                                <li><a href="">¿Por qué debería tener cuidado con mi contraseña maestra?</a></li>
-                                <li><a href="">¿Por qué la contraseña maestra es una larga cadena ininteligible?</a></li>
-                                <li><a href="">¿Para qué sirven mis diferentes llaves?</a></li>
-                                <li><a href="">¿Qué hago si pierdo mi contraseña o claves privadas ?</a></li>
-                                <li><a href="">¿Están asegurados mis tokens CREA y CREA Energy en caso de que alguien haga un pirateo de tu dispositivo o cuenta?</a></li>
-                                <li><a href="">¿Qué puedo hacer si descubro que alguien ha hackeado mi cuenta?</a></li>
-                                <li><a href="">¿Cómo funciona el protocolo de recuperación de cuentas robadas?</a></li>
-                            </ul>
-                            <h5>Desarrolladores</h5>
-                            <ul>
-                                <li><a href="">¿El código Creary.net o de la blockchain de Crea es de código abierto?</a></li>
-                            </ul>
-                            <h5>Testigos</h5>
-                            <ul>
-                                <li><a href="">¿Qué son los testigos de Crea?</a></li>
-                                <li><a href="">¿Cómo puedo votar por los testigos?</a></li>
-                            </ul>
-                            <h5>Diverso</h5>
-                            <ul>
-                                <li><a href="">¿Dónde se anuncian las actualizaciones y noticias relacionadas con Creary y Crea?</a></li>
-                                <li><a href="">¿Dónde puedo pedir ayuda si mi pregunta no fue respondida aquí?</a></li>
-                            </ul>
+                            </ul>-->
+
                         </div>
                     </div>
                 </div>
@@ -169,5 +74,5 @@
         <!--end of container-->
     </section>
 
-
+    <script src="/js/common/faq.js"></script>
     <?php include ('element/footer.php'); ?>
