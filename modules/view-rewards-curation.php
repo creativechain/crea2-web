600400cc69e8989bec4db5c80861bd6bae4c75d2
manolo <manolo@idealis.es>
2018-12-30 20:30:49 +0100

    add title in rewards curation

--- modules/view-rewards-curation.php
+++ modules/view-rewards-curation.php
@@ -1,5 +1,5 @@
 <div class="col-md-12">
-    <h3 class="title-section-profile">{{ lang.PROFILE.CURATION_TITLE }}</h3>
+    <h3 class="title-section-profile">{{ lang.PROFILE.TITLE_CURATION }}</h3>
 </div>
 <div class="col-md-12">
     <div class="boxed boxed--border box-rewards-autor">
