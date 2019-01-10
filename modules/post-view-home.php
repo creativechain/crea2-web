ef0cfdfcfb3fa3e3e75f4593f43aa0138446014b
manolo <manolo@idealis.es>
2019-01-10 11:06:53 +0100

    dark menu right and color links tag in home

--- modules/post-view-home.php
+++ modules/post-view-home.php
@@ -10,37 +10,37 @@
 
         <div class="card__body">
             <h4 v-on:click="openPost(state.content[p])">{{ state.content[p].title }}</h4>
-            <p class="description-post-box" v-html="getTags(state.content[p])"></p>
-            <ul class="list-inline list-unstyled w-100">
-                <li class="li-like">
-                    <like v-on:vote="onVote" v-bind:session="session" v-bind:post="state.content[p]"></like>
-                </li>
-                <li>
-                    <div class="dropdown dropdown-price">
-                        <span class="dropdown__trigger"> {{ getPayout(state.content[p]) }}
-                            <i class="stack-down-open"></i>
-                        </span>
-                        <div class="dropdown__container price">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-12 dropdown__content amount-post-view-home">
-                                        <p class="title">{{ hasPaid(state.content[p]) ? lang.HOME.DROPDOWN_PAST_PAYOUT : lang.HOME.DROPDOWN_PENDING_PAYOUT}} {{ getPayout(state.content[p]) }}</p>
-                                        <p v-if="!hasPaid(state.content[p])">{{ getPendingPayouts(state.content[p]) }}</p>
-                                        <p>{{ getPayoutPostDate(state.content[p]) }}</p>
-                                        <p v-if="hasPromotion(state.content[p])">{{ lang.HOME.PROMOTION_COST }}: {{ getPromotion(state.content[p]) }}</p>
-                                    </div>
-                                </div><!--end row-->
-                            </div><!--end container-->
-                        </div><!--end dropdown container-->
-                    </div>
-                </li>
-                <li class="float-right li-comment">
-                    <a href="">
-                        <img src="/img/crea-web/comments.svg" alt="" />
-                        <span>{{ state.content[p].children }}</span>
-                    </a>
-                </li>
-            </ul>
+            <p class="description-post-box text-truncate" v-html="getTags(state.content[p])"></p>
+                <ul class="list-inline list-unstyled w-100">
+                    <li class="li-like">
+                        <like v-on:vote="onVote" v-bind:session="session" v-bind:post="state.content[p]"></like>
+                    </li>
+                    <li>
+                        <div class="dropdown dropdown-price">
+                            <span class="dropdown__trigger"> {{ getPayout(state.content[p]) }}
+                                <i class="stack-down-open"></i>
+                            </span>
+                            <div class="dropdown__container price">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-md-12 dropdown__content amount-post-view-home">
+                                            <p class="title">{{ hasPaid(state.content[p]) ? lang.HOME.DROPDOWN_PAST_PAYOUT : lang.HOME.DROPDOWN_PENDING_PAYOUT}} {{ getPayout(state.content[p]) }}</p>
+                                            <p v-if="!hasPaid(state.content[p])">{{ getPendingPayouts(state.content[p]) }}</p>
+                                            <p>{{ getPayoutPostDate(state.content[p]) }}</p>
+                                            <p v-if="hasPromotion(state.content[p])">{{ lang.HOME.PROMOTION_COST }}: {{ getPromotion(state.content[p]) }}</p>
+                                        </div>
+                                    </div><!--end row-->
+                                </div><!--end container-->
+                            </div><!--end dropdown container-->
+                        </div>
+                    </li>
+                    <li class="float-right li-comment">
+                        <a href="">
+                            <img src="/img/crea-web/comments.svg" alt="" />
+                            <span>{{ state.content[p].children }}</span>
+                        </a>
+                    </li>
+                </ul>
         </div>
 
         <div class="card__bottom card-show">
