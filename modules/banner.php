57833b9331987f24fcd8681e95e9db99269444c2
ander7agar <ander7agar@gmail.com>
2019-01-11 19:57:18 +0100

    fix banner close link

--- modules/banner.php
+++ modules/banner.php
@@ -1,6 +1,6 @@
 <section v-cloak v-if="showBanner" class="imagebg image--dark cover cover-blocks bg--secondary" id="slide-home">
     <div class="row-close" v-on:click="showBanner = false">
-        <a href="#"><i class="fas fa-times"></i></a>
+        <a class="cursor"><i class="fas fa-times"></i></a>
     </div>
     <div class="background-image-holder">
         <img alt="background" src="/img/crea-web/slide_casmiclab_logo.jpg"/>
