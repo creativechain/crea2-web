59df390b260a64e42521838e029e3f4a30feac27
manolo <manolo@idealis.es>
2019-02-13 23:36:57 +0100

    estructura vista exchange

---
+++ exchange.php
@@ -0,0 +1,125 @@
+<?php include('element/navbar.php'); ?>
+    <div class="main-container exchange mt--3">
+        <div class="container header-exchange">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="head-splits name">
+                        <h4 class="title">TRX<span class="gray-color">/BTC</span></h4>
+                        <p>TRON</p>
+                    </div>
+                    <div class="head-splits">
+                        <p class="small gray-color">Last Price</p>
+                        <p class="amount"><strong><span class="green-color">0.00000467</span> $0.05</strong></p>
+                    </div>
+                    <div class="head-splits">
+                        <p class="small gray-color">24h change</p>
+                        <p class="amount"><strong><span class="red-color">-0.00000467</span> -$0.05</strong></p>
+                    </div>
+                    <div class="head-splits">
+                        <p class="small gray-color">24h High</p>
+                        <p class="amount"><strong><span class="gray-color">0.00000467</span> $0.05</strong></p>
+                    </div>
+                    <div class="head-splits">
+                        <p class="small gray-color">24h Low</p>
+                        <p class="amount"><strong><span class="gray-color">-0.00000467</span> -$0.05</strong></p>
+                    </div>
+                    <div class="head-splits">
+                        <p class="small gray-color">24h Volume</p>
+                        <p class="amount"><strong><span class="gray-color">3,4234</span> BTC</strong></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container content-exchange">
+            <div class="row">
+                <div class="col">
+                    <div class="boxed boxed--border">
+                        <div class="tabs-container" data-content-align="left">
+                            <ul class="tabs">
+                                <li class="active">
+                                    <div class="tab__title">
+                                        <span class="h5">Title</span>
+                                    </div>
+                                    <div class="tab__content">
+                                        Content
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="tab__title">
+                                        <span class="h5">Title</span>
+                                    </div>
+                                    <div class="tab__content">
+                                        Content
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="tab__title">
+                                        <span class="h5">Title</span>
+                                    </div>
+                                    <div class="tab__content">
+                                        Content
+                                    </div>
+                                </li>
+                            </ul>
+                        </div><!--end of tabs container-->
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="boxed boxed--border">
+                        grafica
+                    </div>
+
+                    <div class="boxed boxed--border">
+                        <div class="row">
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <h6 class="title"><strong>Buy TRX</strong><span class="pull-right">BTC: 3.87062</span></h6>
+                                <div class="form-group">
+                                    <label for="">Price</label>
+                                    <input type="number" class="form-control" id="" placeholder="BTC">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Amount</label>
+                                    <input type="text" class="form-control" id="" placeholder="TRX">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Total</label>
+                                    <input type="text" class="form-control" id="" placeholder="BTC">
+                                </div>
+                                <div class="form-group">
+                                    <a class="btn btn-primary">
+                                        <span class="btn__text">BUY</span>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <h6 class="title"><strong>Sell TRX</strong><span class="pull-right">BTC: 3.87062</span></h6>
+                                <div class="form-group">
+                                    <label for="">Price</label>
+                                    <input type="number" class="form-control" id="" placeholder="BTC">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Amount</label>
+                                    <input type="text" class="form-control" id="" placeholder="TRX">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Total</label>
+                                    <input type="text" class="form-control" id="" placeholder="BTC">
+                                </div>
+                                <div class="form-group">
+                                    <a class="btn btn-unfollow color--white">SELL</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="boxed boxed--border">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+<?php include('element/footer.php'); ?>
