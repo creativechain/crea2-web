b40095ec05936a77c5afcbc08abae22a37825227
ander7agar <ander7agar@gmail.com>
2019-02-28 11:24:36 +0100

    fixing nodes

--- js/common/conf.js
+++ js/common/conf.js
@@ -6,7 +6,7 @@
 let local = false;
 
 let apiOptions = {
-    nodes: ['https://crea.owldevelopers.site'],
+    nodes: ['https://nodes.creary.net'],
     apiUrl : 'https://api.creary.net',
     ipfs: 'https://ipfs.creary.net/ipfs/',
     ipfsd: 'https://api.creary.net/ipfs',
