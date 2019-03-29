0a04c9c8a64a66da3fe76c6ed60b5064f5ff9b6b
ander7agar <ander7agar@gmail.com>
2019-03-29 12:29:49 +0100

    adapted for new installation system

--- modules/navbar-filter-home.php
+++
@@ -1,4 +0,0 @@
-<div class="col"></div>
-<div class="col-1 col-md-1 text-right">
-    <div id="view-changer" class="cursor view-all"></div>
-</div>
--- modules/welcome-profile-empty.php
+++
@@ -1,9 +0,0 @@
-<div class="col-md-12">
-    <p>{{ lang.PROFILE.NO_POSTS_TITLE }}</p>
-    <p>{{ lang.PROFILE.NO_POSTS_TEXT }}</p>
-</div>
-<div class="col-md-12 row-options-empty mt-4">
-    <a href="/publish">{{ lang.PROFILE.NO_POSTS_PUBLISH }}</a>
-    <a href="/explore">{{ lang.PROFILE.NO_POSTS_EXPLORE }}</a>
-    <a href="/faq">{{ lang.PROFILE.NO_POSTS_FAQ }}</a>
-</div>
