64d3b180fba03ba24a3e3bd254a599b90cedf395
ander7agar <ander7agar@gmail.com>
2019-04-04 10:02:52 +0200

    implmenting market

--- js/common/market.js
+++ js/common/market.js
@@ -49,7 +49,30 @@
         crea.api.getOrderBook(100, function (err, result) {
             if (!catchError(err)) {
                 //parse order book
-                marketContainer.orderBook = result;
+                var asks = [];
+                var bids = [];
+                result.asks.forEach(function (aks) {
+                    var base = Asset.parse(aks.order_price.base);
+                    var quote = Asset.parse(aks.order_price.quote);
+                    ask.order_price = Asset.parse({amount: base.toFloat() / quote.toFloat(), nai: quote.asset.symbol}).toFloat();
+
+                    ask.crea = Asset.parse({amount: ask.crea, nai: 'crea'}).toFloat();
+                    ask.cbd = Asset.parse({amount: ask.cbd, nai: 'cbd'}).toFloat();
+
+                    asks.push(asks);
+                });
+
+                result.bids.forEach(function (bid) {
+                    var base = Asset.parse(bid.order_price.base);
+                    var quote = Asset.parse(bid.order_price.quote);
+                    bid.order_price = Asset.parse({amount: base.toFloat() / quote.toFloat(), nai: quote.asset.symbol}).toFloat();
+
+                    bid.crea = Asset.parse({amount: bid.crea, nai: 'crea'}).toFloat();
+                    bid.cbd = Asset.parse({amount: bid.cbd, nai: 'cbd'}).toFloat();
+
+                    bids.push(bid);
+                });
+                marketContainer.orderBook = {asks: asks, bids: bids};
                 marketContainer.$forceUpdate();
             }
         })
@@ -73,6 +96,131 @@
             loadRecentTrades();
         }, 3000);
     }
+
+    creaEvents.on('crea.dom.ready', function () {
+
+        $('#buy_left_result_all').DataTable({
+            bFilter: false,
+            bInfo: false,
+            "lengthChange": false,
+            aoColumnDefs : [ {
+                orderable : false,
+                aTargets : ['_all']
+            }],
+            order: [],
+            "scrollY":        "250px",
+            "scrollCollapse": true,
+            "paging":         false
+        });
+        $('#sell_left_result_all').DataTable({
+            bFilter: false,
+            bInfo: false,
+            "lengthChange": false,
+            aoColumnDefs : [ {
+                orderable : false,
+                aTargets : ['_all']
+            }],
+            order: [],
+            "scrollY":        "250px",
+            "scrollCollapse": true,
+            "paging":         false
+        });
+        $('#buy-left_all').DataTable({
+            bFilter: false,
+            bInfo: false,
+            "lengthChange": false,
+            aoColumnDefs : [ {
+                orderable : false,
+                aTargets : ['_all']
+            }],
+            order: [],
+            "paging":         false
+        });
+
+        $('#buy-left').DataTable({
+            bFilter: false,
+            bInfo: false,
+            "lengthChange": false,
+            aoColumnDefs : [ {
+                orderable : false,
+                aTargets : ['_all']
+            }],
+            order: [],
+            "paging":false,
+            "bAutoWidth": false,
+            "aoColumns" : [
+                { sWidth: '25%' },
+                { sWidth: '25%' },
+                { sWidth: '25%' },
+                { sWidth: '25%' }
+            ]
+        });
+        $('#buy_left_result').DataTable({
+            bFilter: false,
+            bInfo: false,
+            "lengthChange": false,
+            aoColumnDefs : [ {
+                orderable : false,
+                aTargets : ['_all']
+            }],
+            order: [],
+            "scrollY":        "400px",
+            "scrollCollapse": true,
+            "paging":         false
+        });
+        $('#sell-left').DataTable({
+            bFilter: false,
+            bInfo: false,
+            "lengthChange": false,
+            aoColumnDefs : [ {
+                orderable : false,
+                aTargets : ['_all']
+            }],
+            order: [],
+            "paging":         false
+        });
+        $('#sell_left_result').DataTable({
+            bFilter: false,
+            bInfo: false,
+            "lengthChange": false,
+            aoColumnDefs : [ {
+                orderable : false,
+                aTargets : ['_all']
+            }],
+            order: [],
+            "scrollY":        "400px",
+            "scrollCollapse": true,
+            "paging":         false
+        });
+
+
+        $('#example1').DataTable({
+            bFilter: false,
+            bInfo: false,
+            "lengthChange": false,
+            aoColumnDefs : [ {
+                orderable : false,
+                aTargets : ['_all']
+            }],
+            order: [],
+            "scrollY":        "534px",
+            "scrollCollapse": true,
+            "paging":         false
+        });
+        $('#example2').DataTable({
+            bFilter: false,
+            bInfo: false,
+            "lengthChange": false,
+            aoColumnDefs : [ {
+                orderable : false,
+                aTargets : ['_all']
+            }],
+            order: [],
+            "scrollY":        "200px",
+            "scrollCollapse": true,
+            "paging":         false
+        });
+    })
 
     creaEvents.on('crea.session.login', function (s, a) {
         setUp(s, a);
